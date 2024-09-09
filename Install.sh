959d990c78bfecdb8f2dc0b9dcaf7332a27c0749
Wproulx09 <112491010+Wproulx09@users.noreply.github.com>
2024-09-09 11:22:37 -0400

    Add files via upload
    kitty configuration and Install.sh setup

---
+++ Install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#Creating workspace
+my_workspace=("bin" "cegep" "projets")
+
+#Loop create directories
+for dir in "${my_workspace[@]}"; do
+	echo $dir
+	mkdir ~/$dir
+done
+
+#Creating an array of app to install
+my_app=("kitty" "neovim" "openvpn" "github-cli" "git"
+	"gcc" "gdb" "python3" "python-pip" "python-virtualenv" 
+	"polybar")
+
+#Loop install my_app
+for app in "${my_app[@]}"; do
+	echo -e "\n\e[32;40mInstalling $app\e[0m"
+	yes | sudo pacman -S $app
+done
